7eb306c8c966a9f2a8df5c881e3b84f7631796df
nandagiriakhila <134784749+nandagiriakhila@users.noreply.github.com>
2025-06-25 11:36:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+<div>
+<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  chat-icon="https:&#x2F;&#x2F;i.postimg.cc&#x2F;T356z0Mm&#x2F;bot.png"
+  intent="WELCOME"
+  chat-title="Querybot"
+  agent-id="4b284695-c335-44c7-9718-e748f9960fa1"
+  language-code="en"
+></df-messenger>
+</div>
